64abb53793d14f7132226f26dcd4a6260f6a4131
Creature Feature <ivy.mccurdy@gmail.com>
2023-05-15 00:27:26 -0500

    added more questions to questions file, took out console log on app file, created another test md file

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function askQuestions() {
 
 
 function main() {
-    console.log(questions);
+    // console.log(questions);
     askQuestions();
 }
 
--- questions.js
+++ questions.js
@@ -3,7 +3,31 @@ const questions = [
         type: "input",
         message: "What is the name of your project?",
         name: "projectName",
-        default: "README GENERATOR"
+    },
+    {
+        type: "input",
+        message: "How would you describe this project?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "How would you install this app?",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "How do you use this app?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "How could someone help contribute to this project?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "What are some tests that you've made for this app?",
+        name: "tests"
     }
 ];
 
