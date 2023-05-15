440f42fc82e40f8851cededcb7f2dfe21a7c4e94
Creature Feature <ivy.mccurdy@gmail.com>
2023-05-14 23:29:24 -0500

    added import, called on questions.js in app.js, created markdown file, tested fs and created a test.md file, created folder for markdown file

---
+++ app.js
@@ -0,0 +1,21 @@
+const { questions } = require("./questions");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+function askQuestions() {
+    inquirer.prompt(questions).then(({projectName: project}) => {
+        console.log(project);
+        fs.appendFile("test.md", JSON.stringify(project), "utf-8", function(err) {
+            if (err) console.error(err);
+        });
+    });
+}
+
+
+function main() {
+    console.log(questions);
+    askQuestions();
+}
+
+main();
---
+++ questions.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "projectName",
+        default: "README GENERATOR"
+    }
+];
+
+module.exports = {
+    questions
+}
